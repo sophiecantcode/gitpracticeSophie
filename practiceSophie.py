196d5e31cf269912fe793fd6b585a173ae5a5b53
laurenshareshian <laurenshareshian@gmail.com>
2020-02-26 11:16:46 -0800

    lauren added her function

--- practiceSophie.py
+++ practiceSophie.py
@@ -1,18 +1,22 @@
 #Partner 1: Lauren Shareshian
 #Partner 2: Sophie Chen
 #################
-assignment name: github practice 20 points, 2/26/20
+# assignment name: github practice 20 points, 2/26/20
 
 def getNRandom(n):
-	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
-    pass
+   '''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
+   pass
 
 def multiplyRandom(numbers):
-	'''takes in a list of n numbers and returns the product of the numbers'''
-    pass
+   '''takes in a list of n numbers and returns the product of the numbers'''
+   product = 1
+   for number in numbers:
+      product = product*number
+   return product
 
 def main():
-	print(multiplyRandom(getNRandom(10))
+   print(multiplyRandom(getNRandom(10)))
+
 
 if __name__ == "__main__":
-	main()
+   main()
