ccd01489714059485acc551f58562168de37a8e6
Sophie Chen <chens@go.oes.edu>
2020-02-26 11:18:58 -0800

    get random function added

--- practiceSophie.py
+++ practiceSophie.py
@@ -3,9 +3,15 @@
 #################
 assignment name: github practice 20 points, 2/26/20
 
+from random import randint
+
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
-    pass
+    number_list = []
+    for i in range(n):
+        value = randint(1,10)
+        number_list.append(value)
+    return number_list
 
 def multiplyRandom(numbers):
 	'''takes in a list of n numbers and returns the product of the numbers'''
